d640502ae45bd8d12264dea4716fc4da34914f5c
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-09-18 21:01:41 -0700

    Initialize fb_chatbot

--- furballbot/furballbot/settings.py
+++ furballbot/furballbot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y6d#cswond(=4846_vin99))564*18dw7fdgpy@falb&-l&2l1'
+SECRET_KEY = 'p7_@49fm+=%p&np#jd6owjmxsl6r!gpc3e&wg1$#0af)r=a&n7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
