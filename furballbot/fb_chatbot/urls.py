6525ec2db3683317d21dd1574ad1bd344e1bab48
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2016-09-18 23:41:15 -0700

    Add FurBallBotView

--- furballbot/fb_chatbot/urls.py
+++ furballbot/fb_chatbot/urls.py
@@ -1,3 +1,7 @@
 from django.conf.urls import url, include
+from .views import FurBallBotView
 
-urlpatterns = []
+urlpatterns = [
+	# webhook: print binascii.hexlify(os.urandom(25))
+	url(r'^4071858ca401f905fb75de5417e28e68857a4fc90cdbc49291/?$', FurBallBotView.as_view()),
+]
--- furballbot/fb_chatbot/views.py
+++ furballbot/fb_chatbot/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+class FurBallBotView(generic.View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("Furball: Meow!")
