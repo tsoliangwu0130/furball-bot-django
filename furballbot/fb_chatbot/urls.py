b5ee0b4e130aa9fdd59d6e486b825ab9644a6017
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-01-23 17:14:22 -0800

    Reset project

--- furballbot/fb_chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url, include
-from .views import FurBallBotView
-
-urlpatterns = [
-	# webhook: print binascii.hexlify(os.urandom(25))
-	url(r'^4071858ca401f905fb75de5417e28e68857a4fc90cdbc49291/?$', FurBallBotView.as_view()),
-]
--- furballbot/fb_chatbot/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-from django.http.response import HttpResponse
-
-
-class FurBallBotView(generic.View):
-	def get(self, request, *args, **kwargs):
-		return HttpResponse("Furball: Meow!")
