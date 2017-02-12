acd7d36ce5cc14226ddcaf112061e727d6601bb9
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-02-12 14:24:51 -0800

    Add post_facebook_message

--- furball_bot_project/furball_bot_app/views.py
+++ furball_bot_project/furball_bot_app/views.py
@@ -9,6 +9,16 @@ from django.http.response import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
+PAGE_ACCESS_TOKEN = 'EAAFe4BILMB0BAO2Sua74SdRFWF6Uno5FmlIDthU3hHpZA7eJOYOf2FfjbZBOXJSOe6YpbNaj1LNHGfk59aOmUioe2DD4NZC1oKgCxVycZA8cufB1ge53PiGeq4jB6uD6eSWLLPXMqWRPPaPKy8P4OF7nVGiBamhZANkVR4KwpTgZDZD'
+VERIFY_TOKEN = 'furball_bot_token'
+
+
+def post_facebook_message(fbid, recevied_message):
+	post_message_url = 'https://graph.facebook.com/v2.8/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
+	response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": recevied_message}})
+	status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
+	pprint(status.json())
+
 
 class FurballBotView(generic.View):
 	# The get method is the same as before.. omitted here for brevity
@@ -17,7 +27,7 @@ class FurballBotView(generic.View):
 		return generic.View.dispatch(self, request, *args, **kwargs)
 
 	def get(self, request, *args, **kwargs):
-		if self.request.GET['hub.verify_token'] == 'furball_bot_token':
+		if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
 			return HttpResponse(self.request.GET['hub.challenge'])
 		else:
 			return HttpResponse('Error, invalid token')
@@ -35,4 +45,7 @@ class FurballBotView(generic.View):
 				if 'message' in message:
 					# Print the message to the terminal
 					pprint(message)
+					# Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
+					# are sent as attachments and must be handled accordingly.
+					post_facebook_message(message['sender']['id'], message['message']['text'])
 		return HttpResponse()
