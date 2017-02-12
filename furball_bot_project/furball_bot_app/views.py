deb3c7defca67c5bcf70d6f9f0b144b07520364b
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-02-12 14:03:00 -0800

    Add post method

--- furball_bot_project/furball_bot_app/views.py
+++ furball_bot_project/furball_bot_app/views.py
@@ -5,8 +5,28 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 class FurballBotView(generic.View):
+	# The get method is the same as before.. omitted here for brevity
+	@method_decorator(csrf_exempt)
+	def dispatch(self, request, *args, **kwargs):
+		return generic.View.dispatch(self, request, *args, **kwargs)
+
 	def get(self, request, *args, **kwargs):
 		if self.request.GET['hub.verify_token'] == 'furball_bot_token':
 			return HttpResponse(self.request.GET['hub.challenge'])
 		else:
 			return HttpResponse('Error, invalid token')
+
+	# Post function to handle Facebook messages
+	def post(self, request, *args, **kwargs):
+		# Converts the text payload into a python dictionary
+		incoming_message = json.loads(self.request.body.decode('utf-8'))
+		# Facebook recommends going through every entry since they might send
+		# multiple messages in a single call during high load
+		for entry in incoming_message['entry']:
+			for message in entry['messaging']:
+				# Check to make sure the received call is a message call
+				# This might be delivery, optin, postback for other events
+				if 'message' in message:
+					# Print the message to the terminal
+					pprint(message)
+		return HttpResponse()
