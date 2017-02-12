76484c6beb9349eebd880c91154ebad2f973dddf
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-02-12 14:04:26 -0800

    Add needed import lib

--- furball_bot_project/furball_bot_app/views.py
+++ furball_bot_project/furball_bot_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import generic
 from django.http.response import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
