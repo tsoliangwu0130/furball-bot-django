bece5a26b9dd2f706757e2fae5e5d52814ff82ce
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-02-11 23:03:19 -0800

    Add furball_bot app first view

--- furball_bot_project/furball_bot_app/urls.py
+++ furball_bot_project/furball_bot_app/urls.py
@@ -1,3 +1,6 @@
 from django.conf.urls import include, url
+from .views import FurballBotView
 
-urlpatterns = []
+urlpatterns = [
+	url(r'^c97ea760afab571005f7996878f1ba7873417d8de500a86442/?$', FurballBotView.as_view()),
+]
--- furball_bot_project/furball_bot_app/views.py
+++ furball_bot_project/furball_bot_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http.response import HttpResponse
+
 
 # Create your views here.
+class FurballBotView(generic.View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("Furball: Meow!")
