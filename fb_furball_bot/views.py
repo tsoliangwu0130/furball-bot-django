df5fb2bf35e4d2829eb8cbaf494c8936548efc97
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-02-16 13:42:11 -0800

    Import secrets from django setting

--- fb_furball_bot/views.py
+++ fb_furball_bot/views.py
@@ -4,14 +4,12 @@ import re
 import requests
 from pprint import pprint
 
-from django.views import generic
+from django.conf import settings
 from django.http.response import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
+from django.views import generic
+from django.views.decorators.csrf import csrf_exempt
 
-
-VERIFY_TOKEN = "furball_bot_token"
-PAGE_ACCESS_TOKEN = "EAAFe4BILMB0BAG9IHy5FZBpdywkLzrqK1UmPXTFQSYMdgYkuZABC0hEulEvsswxqHFhpcrDC4cJWqwEeS3O5noQ3ps4alyFiVwHfeKsH4eNF9nM670YU03zvuOCZAZAUY8U04nB28eQ5YpaXn1LAKY7UXOeS5mAIAYRjXAgZBLwZDZD"
 
 jokes = {'stupid': ["""Yo' Mama is so stupid, she needs a recipe to make ice cubes.""",
 					"""Yo' Mama is so stupid, she thinks DNA is the National Dyslexics Association."""],
@@ -32,7 +30,7 @@ def post_facebook_message(fbid, recevied_message):
 		joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!"
 
 	user_details_url = "https://graph.facebook.com/v2.8/%s" % fbid
-	user_details_params = {'fields': 'first_name,last_name,profile_pic', 'access_token': PAGE_ACCESS_TOKEN}
+	user_details_params = {'fields': 'first_name,last_name,profile_pic', 'access_token': settings.PAGE_ACCESS_TOKEN}
 	user_details = requests.get(user_details_url, user_details_params).json()
 
 	try:
@@ -40,7 +38,7 @@ def post_facebook_message(fbid, recevied_message):
 	except KeyError:
 		joke_text = 'Caught KeyError...' + joke_text
 
-	post_message_url = 'https://graph.facebook.com/v2.8/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
+	post_message_url = 'https://graph.facebook.com/v2.8/me/messages?access_token=%s' % settings.PAGE_ACCESS_TOKEN
 	response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": joke_text}})
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
 	pprint(status.json())
@@ -48,7 +46,7 @@ def post_facebook_message(fbid, recevied_message):
 
 class FurballBotView(generic.View):
 	def get(self, request, *args, **kwargs):
-		if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+		if self.request.GET['hub.verify_token'] == settings.VERIFY_TOKEN:
 			return HttpResponse(self.request.GET['hub.challenge'])
 		else:
 			return HttpResponse('Error, invalid token')
