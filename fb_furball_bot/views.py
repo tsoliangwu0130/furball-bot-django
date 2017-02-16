2bc83c734a42fa95e60e18cfba170a2d97d32392
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-02-15 19:48:46 -0800

    Add facebook verify token

--- fb_furball_bot/views.py
+++ fb_furball_bot/views.py
@@ -2,6 +2,13 @@ from django.views import generic
 from django.http.response import HttpResponse
 
 
+VERIFY_TOKEN = "furball_bot_token"
+PAGE_ACCESS_TOKEN = ""
+
+
 class FurballBotView(generic.View):
 	def get(self, request, *args, **kwargs):
-		return HttpResponse("Hello World!")
+		if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+			return HttpResponse(self.request.GET['hub.challenge'])
+		else:
+			return HttpResponse('Error, invalid token')
