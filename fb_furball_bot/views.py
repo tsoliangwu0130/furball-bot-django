33c0409cbe5d04cdbc466d289c6e1a40bcff228d
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-02-15 23:50:03 -0800

    Add customize msg

--- fb_furball_bot/views.py
+++ fb_furball_bot/views.py
@@ -30,6 +30,11 @@ def post_facebook_message(fbid, recevied_message):
 			break
 	if not joke_text:
 		joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!"
+
+	user_details_url = "https://graph.facebook.com/v2.8/%s" % fbid
+	user_details_params = {'fields': 'first_name,last_name,profile_pic', 'access_token': PAGE_ACCESS_TOKEN}
+	user_details = requests.get(user_details_url, user_details_params).json()
+	joke_text = 'Yo ' + user_details['first_name'] + '..! ' + joke_text
 	post_message_url = 'https://graph.facebook.com/v2.8/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
 	response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": joke_text}})
 	status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
